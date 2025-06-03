dca46c5aa0acef886236b259d784d0930303e86d
Devrat Dave <devratdave@MacBookAir.mshome.net>
2025-06-03 12:35:38 +0530

    added script tag in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="mW-G1gU8o2frJxDfhYgdBM6ND9H9Z8BrwrBha1LHCHQ" />
         <meta name="google-adsense-account" content="ca-pub-2364704847990103" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2364704847990103" crossOrigin="anonymous" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
