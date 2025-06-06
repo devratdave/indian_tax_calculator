bf2b266b9a8f1440a863a119a6f5c10382a52f0c
Devrat Dave <devratdave@Devrats-MacBook-Air.local>
2025-06-06 11:18:49 +0530

    db related

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@components/db/index"
+import { prisma } from "@components/app/lib/db"
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
--- db/prisma/migrations/20250528084619_first_prisma_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Comments" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "comment" TEXT NOT NULL,
-
-    CONSTRAINT "Comments_pkey" PRIMARY KEY ("id")
-);
